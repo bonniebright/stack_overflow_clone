60297a98afed02bde25db76e0658063d41118799
Patti Jurgensen & Bonnie Bright <student@epicodus.com>
2014-04-14 11:28:37 -0700

    add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
+  has_secure_password
 
   has_many :questions
   has_many :votes
   has_many :answers
+
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+            presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true,
+            case_sensitive: false
+  validates :username,
+            presence: true,
+            uniqueness: true,
+            case_sensitive: false
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_digest, presence: true, uniqueness: true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ StackOverflow::Application.routes.draw do
 
   root to: 'questions#index'
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :questions
   resources :users
+  resources :sessions
 
 end
